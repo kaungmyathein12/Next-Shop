4e842e86d6f2bcccbee13529d27f0bc6d010c088
kaungmyathein12 <kaungmyathein.dev@gmail.com>
2023-01-04 22:20:05 +0630

    setup next auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,23 @@
 import NextAuth from "next-auth/next";
 import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { RequestInternal, Awaitable, User } from "next-auth";
 
 export const authOptions = {
   providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: function (
+        credentials: Record<"username" | "password", string> | undefined,
+        req: Pick<RequestInternal, "body" | "query" | "headers" | "method">
+      ): Awaitable<User | null> {
+        throw new Error("Function not implemented.");
+      },
+    }),
     GitHubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,48 @@
 import { getSession, signOut, useSession } from "next-auth/react";
-import { useEffect } from "react";
+import Image from "next/image";
+import { useState } from "react";
+import Ripples from "react-ripples";
 
 export default function Home() {
   const { data: session } = useSession();
+  const [showMenu, setShowMenu] = useState<boolean>(false);
   return (
     <div>
-      <nav className="border px-4 py-[10px]">
-        <h3 className="text-xl font-semibold">
-          NEXT Shop {session?.user?.email}
-        </h3>
-        <button onClick={() => signOut()}>Sign Out</button>
-      </nav>
+      <div className="w-[900px] mx-auto mt-16">
+        <nav className="px-4 flex flex-row justify-between items-start">
+          <h3 className="text-xl font-semibold">NEXT Shop</h3>
+          <div
+            className="w-[30px] h-[30px] relative cursor-pointer"
+            onBlur={() =>
+              setTimeout(() => {
+                setShowMenu(false);
+              }, 300)
+            }
+            tabIndex={0}
+          >
+            <Image
+              src={session?.user?.image as string}
+              alt={session?.user?.name as string}
+              width={30}
+              height={30}
+              onClick={() => setShowMenu(!showMenu)}
+              className="rounded-md"
+            />
+            {showMenu && (
+              <div className="absolute top-[120%] w-[77.5px] right-0 border">
+                <Ripples>
+                  <button
+                    className="text-sm bg-[#ededed] px-3 py-1 rounded-md"
+                    onClick={() => signOut()}
+                  >
+                    Log Out
+                  </button>
+                </Ripples>
+              </div>
+            )}
+          </div>
+        </nav>
+      </div>
     </div>
   );
 }
