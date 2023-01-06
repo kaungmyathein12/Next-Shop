4468d833d107967e025c0316485c03ee0e048e8d
kaungmyathein12 <kaungmyathein.dev@gmail.com>
2023-01-06 21:31:40 +0630

    fix

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -8,9 +8,13 @@ const NavBar = () => {
   const [showMenu, setShowMenu] = useState<boolean>(false);
 
   return (
-    <nav className="fixed w-full bg-white flex flex-row justify-between items-center border-b px-8 py-[10px] space-x-4 flex-shrink-0">
+    <nav className="fixed w-full bg-white flex flex-row justify-between items-center border-b px-8 py-[10px] space-x-4 flex-shrink-0 z-[20]">
       <h3 className="text-xl font-semibold">NEXT Shop</h3>
-      <div className="grow"></div>
+      <div className="grow flex flex-row justify-end items-center space-x-5 uppercase pr-[84px]">
+        <span className="text-[14px] font-medium">Home</span>
+        <span className="text-[14px] font-medium">Employee Account</span>
+        <span className="text-[14px] font-medium">Add New Product</span>
+      </div>
       <div
         className="relative cursor-pointer flex flex-row justify-start items-start space-x-2"
         onBlur={() =>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { getSession } from "next-auth/react";
 import riceroll from "../assets/img/riceroll.png";
 import sushi from "../assets/img/sushi.png";
+import food0 from "../assets/img/food0.jpeg";
+import food1 from "../assets/img/food1.jpeg";
 
 import Image from "next/image";
 
@@ -62,9 +64,9 @@ export default function Home() {
   return (
     <div className="mx-auto flex flex-row justify-start items-start flex-shrink-0 font-poppins">
       <div className="border-r w-[78%] flex-shrink-0 h-screen pl-8">
-        <div className="mt-[80px]">
+        <div className="pt-[80px] sticky top-0  bg-white z-[10]">
           <h4 className="text-[18px] font-medium">Categories</h4>
-          <div className="mt-5">
+          <div className="mt-5 pb-[30px]">
             <div className="flex flex-row justify-start items-center space-x-4 w-[100%] overflow-auto scrollbar-hide pr-6">
               <div className="w-[100px] h-[100px] bg-black flex-shrink-0 rounded">
                 <div className="w-[100%] h-[60%] grid place-items-center">
@@ -99,8 +101,29 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="mt-[50px]">
+        <div className="mt-[20px] pb-[40px]">
           <h4 className="text-[18px] font-medium">Special menu for you</h4>
+          <div className="mt-8 mr-6 grid grid-cols-4 gap-8">
+            {[1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15].map(
+              (item: any, index: any) => (
+                <div key={index} className="hover:scale-105 transition-all">
+                  <div className="w-full h-[150px] relative overflow-hidden">
+                    <Image
+                      src={food0}
+                      width={350}
+                      height={150}
+                      alt="k"
+                      className=" object-cover object-center"
+                    />
+                  </div>
+                  <div className="mt-2">
+                    <h1 className="text-sm mb-1">Ramen</h1>
+                    <span className="text-lg font-medium">$ 8.54</span>
+                  </div>
+                </div>
+              )
+            )}
+          </div>
         </div>
       </div>
       <div className="grow flex-shrink-0 px-4"></div>
